e0e6a50319c1c330a2ec80e7bf7544f3eb22306f
Vincent <4919840+vincent-octo@users.noreply.github.com>
2024-04-29 12:31:59 +0300

    add stats: 90% data coverage / OMOP ID group

--- src/kanta_lab_omop_qc/merge_harmonization_table.py
+++ src/kanta_lab_omop_qc/merge_harmonization_table.py
@@ -6,37 +6,24 @@ import polars as pl
 
 DESCRIPTION = (
     "I take the Kanta lab harmonization table from Google Sheet as a CSV, and "
-    "the FinRegistry Kanta stats on the lab values. From this I add the Top 10 "
-    "lab values for each row of the harmonization table."
+    "add new columns to it."
 )
 
 
 def main():
     args = cli_init()
 
-    merged_table = add_stats_to_table(
-        args.harmonization_table,
-        args.finregistry_stats
-    )
-
-    merged_table.write_csv(
-        args.output,
-    )
+    args.func(**vars(args))
 
 
 def cli_init():
     parser = ArgumentParser(description=DESCRIPTION)
 
+    subparsers = parser.add_subparsers()
+
     parser.add_argument(
         "--harmonization-table",
         help="path to the harmonization table (CSV)",
-        type=Path,
-        required=True
-    )
-
-    parser.add_argument(
-        "--finregistry-stats",
-        help="path to FinRegistry stats on Kanta lab values (CSV)",
         type=Path,
         required=True
     )
@@ -48,14 +35,32 @@ def cli_init():
         required=True
     )
 
+    parser_top10_lab_values = subparsers.add_parser(
+        "add_top10_lab_values",
+        help="Add the top 10 lab values for each row."
+    )
+    parser_top10_lab_values.add_argument(
+        "--finregistry-stats",
+        help="path to FinRegistry stats on Kanta lab values (CSV)",
+        type=Path,
+        required=True
+    )
+    parser_top10_lab_values.set_defaults(func=add_top10_lab_values)
+
+    parser_keep_90percent = subparsers.add_parser(
+        "keep_90percent",
+        help="Filter out rows that don't make 90%% of the data for each OMOP ID"
+    )
+    parser_keep_90percent.set_defaults(func=filter_90percent)
+
     args = parser.parse_args()
 
     return args
 
 
-def add_stats_to_table(table, stats):
+def add_top10_lab_values(*, harmonization_table, finregistry_stats, output, **_kwargs):
     dataf_table = pl.read_csv(
-        table,
+        harmonization_table,
         dtypes={
             "OMOP Concept ID": pl.String,
             "OMOP Concept name": pl.String,
@@ -73,7 +78,7 @@ def add_stats_to_table(table, stats):
 
     dataf_stats = (
         pl.read_csv(
-            stats,
+            finregistry_stats,
             dtypes={
                 "OMOP_ID": pl.String,
                 "LAB_ID": pl.String,
@@ -104,7 +109,7 @@ def add_stats_to_table(table, stats):
         )
     )
 
-    merged_table = (
+    (
         dataf_table
         .join(
             other=dataf_stats,
@@ -122,9 +127,50 @@ def add_stats_to_table(table, stats):
                 "LAB_UNIT"
             ]
         )
+        .write_csv(output)
     )
 
-    return merged_table
+
+def filter_90percent(*, harmonization_table, output, **_kwargs):
+    (
+        pl.scan_csv(
+            harmonization_table,
+            dtypes={
+                "OMOP Concept ID": pl.String,
+                "N Records": pl.UInt64
+            }
+        )
+        .with_columns(
+            # Add row identifier so we can recover the original sorting later on.
+            pl.col("OMOP Concept ID").cum_count().alias("rowid")
+        )
+        .with_columns(
+            # Further stats depends on each OMOP group being sorted max to min.
+            pl.col("N Records").sort(descending=True).over("OMOP Concept ID")
+        )
+        .with_columns(
+            pl.col("N Records").sum().over("OMOP Concept ID").alias("Total_per_omopid"),
+            pl.col("N Records").cum_sum().over("OMOP Concept ID").alias("Cumsum_per_omopid")
+        ).with_columns(
+            (
+                100 * pl.col("Cumsum_per_omopid") / pl.col("Total_per_omopid")
+            ).alias("CumPercentage")
+        )
+        .with_columns(
+            (pl.col("CumPercentage") < 90).alias("Below_threshold?")
+        )
+        .with_columns(
+            pl.col("Below_threshold?").shift(n=1, fill_value=True).over("OMOP Concept ID").alias("Include_first_to_90percent")
+        )
+        .sort("rowid")
+        .select(
+            pl.col("OMOP Concept ID"),
+            pl.col("N Records"),
+            pl.col("Include_first_to_90percent")
+        )
+        .collect()
+        .write_csv(output)
+    )
 
 
 if __name__ == '__main__':
